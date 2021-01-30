ec2527101c2720190b65cfec84d94fbf4b4a8514
Administrator <admin@example.com>
2021-01-30 20:00:12 +0800

    update database

--- application/database.php
+++ application/database.php
@@ -15,7 +15,7 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'lmsys',
+    'database'        => 'txkj',
     // 用户名
     'username'        => 'yeyifu',
     // 密码
